23afc7604eb116a76e65b604cbb6ae947acfbf67
orzchen <61723283+orzchen@users.noreply.github.com>
2023-09-08 16:11:17 +0800

    Update GetAddr4Json.py
    更新了提取IP地址的脚本

--- GetAddr4Json.py
+++ GetAddr4Json.py
@@ -2,24 +2,43 @@ import re
 import socket
 import json
 
+# 保存地址的文件 IP文件和地址文件就是一行一行对应的
+out_addr = "./addrs.txt"
+
 # 打开JSON文件以供读取
 with open('IPv4.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-print('json'+str(len(data)))
-# 打开要查询的IP列表
-with open("res_ip_noport.txt", 'r') as file:
+dataIP = [d['ip'] for d in data]
+
+print('json: '+str(len(data)))
+
+ipList = []
+with open("C:/Users/iCHEN/Desktop/IPAddr/IP.txt", 'r') as file:
     lines = file.readlines()
-print('ip'+str(len(lines)))
+    for line in lines:
+    	ipList.append(str(line).strip())
+
+print('ip数: '+str(len(ipList)))
+
+
 ip_addr = {}
-pattern = r'.*Failed.*'
-# 输出
-with open("./addrs.txt","w+") as f:
+fail_count = 0
+succ_count = 0
+with open(out_addr,"w+") as f:
 	# 遍历每一行
-	for line in lines:
-		for d in data :
-			if d["ip"]+'\n'==line:
-				ip_addr[line] = d["Country"]
-				f.write(d["Country"]+'\n')
-				break
-			
-print(len(ip_addr))
+	for line in ipList:
+		if line in dataIP:
+			for d in data :
+				if d["ip"] ==line:
+					ip_addr[line] = d["Country"]
+					f.write(d["Country"]+'\n')
+					succ_count = succ_count + 1
+					break
+		else:
+			f.write("Failed\n")
+			fail_count = fail_count + 1
+
+		
+# print("查询到IP地址{0}条".format(len(ip_addr)))
+print("查询到IP地址{0}条".format(succ_count))
+print("查询失败{0}条".format(fail_count))
